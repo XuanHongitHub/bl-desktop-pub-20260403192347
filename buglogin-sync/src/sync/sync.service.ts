14f4b7c24f2b3ee51a61b2f952a3224371f703d3
XuanHongitHub <xuanhongithub@users.noreply.github.com>
2026-03-20 19:04:30 +0700

    chore: triggers build

--- buglogin-sync/src/sync/sync.service.ts
+++ buglogin-sync/src/sync/sync.service.ts
@@ -62,7 +62,8 @@ export class SyncService implements OnModuleInit {
     const forcePathStyle =
       this.configService.get<string>("S3_FORCE_PATH_STYLE") !== "false";
 
-    this.bucket = this.configService.get<string>("S3_BUCKET") || "buglogin-sync";
+    this.bucket =
+      this.configService.get<string>("S3_BUCKET") || "buglogin-sync";
 
     this.s3Client = new S3Client({
       endpoint,
